ad7c869bc46bde5c2c78dc6f3aed1469a9a0d932
NotMyPersonalAccount <kevinzj710@gmail.com>
2022-09-08 14:16:16 -0700

    module_3/basic_iteration.py: add instructions

--- module_3/basic_iteration.py
+++ module_3/basic_iteration.py
@@ -1,3 +1,11 @@
+'''
+Start with a list of numbers (you can use ints or floats) assigned to a variable.
+
+Then, iterate over the list, printing out each element. 
+
+Use while loop iteration!
+'''
+
 list = [5, 12, 50, -1]
 i = 0
 while i != len(list):
