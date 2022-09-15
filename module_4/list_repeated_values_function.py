3e0568db4d1a50a422c738aeed982030d4b82d83
NotMyPersonalAccount <kevinzj710@gmail.com>
2022-09-15 15:27:59 -0700

    module_4: further optimize checking for repeat value

--- module_4/list_repeated_values_function.py
+++ module_4/list_repeated_values_function.py
@@ -13,10 +13,12 @@ If the function is called with the argument [0, 1, "hello," 3.5, "hello"], retur
 '''
 
 def has_repeat_values(list):
-	for i in range(len(list) - 1):
-		for j in range(i+1, len(list)):
-			if list[i] == list[j]:
+	found = []
+	for i in list:
+		for j in found:
+			if i == j:
 				return True
+		found.append(i)
 	return False
 
 print(has_repeat_values([0, 1, "hello", 3.5]))
